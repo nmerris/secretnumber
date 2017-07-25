2f7b57a68405ec56896566cd93601ceb95bcafd7
nmerris <nate.merris@gmail.com>
2017-07-25 15:17:40 -0400

    Add user input validation

--- src/mypackage/GuessSecretNumber.java
+++ src/mypackage/GuessSecretNumber.java
@@ -17,27 +17,43 @@ public class GuessSecretNumber {
 		int secretNum = 1 + r.nextInt(10);
 //		System.out.println("secretNum is: " + secretNum);
 		
-		// keep asking the user for a new guess until they guess it, or say N or n to quit
+		// keep asking the user for a new guess until they get it, or types N or n to quit
 		do {
-			System.out.println("Enter your number: ");
-			num = scanner.nextInt();
 			
-			if(num > secretNum) { // entered num is too high
-				System.out.println("You guessed too high, try again? (Y/N)");
-				continuePlaying = scanner.next();
-				if(continuePlaying.equalsIgnoreCase("n"))
-					userGaveUp = true;
-			}
-			if(num < secretNum) { // entered num is too low
-				System.out.println("You guessed too low, try again? (Y/N)");
-				continuePlaying = scanner.next();
-				if(continuePlaying.equalsIgnoreCase("n"))
-					userGaveUp = true;
-			}
+			// keep asking the user for a guess until they enter a valid integer
+			// the only way to get out of this loop is if a valid integer is entered
+			boolean invalidNumberEntered;
+			do {
+				invalidNumberEntered = false;
+				System.out.println("Enter your number: ");
+				try {
+					num = scanner.nextInt();
+				} catch(Exception e) {
+					invalidNumberEntered = true;
+					num = 0; // need this so it will compile
+					System.out.println("Please enter an integer");
+				};
+			} while(invalidNumberEntered);
+			
+			
+			
+			
 			if(num == secretNum) { // secret number has been found
 				System.out.println("CONGRATULATIONS, YOU GUESSED IT!");
 				continuePlaying = "n"; // end the program
 				userGaveUp = false;
+			}
+			else {
+				if(num > secretNum) // entered num is too high
+					System.out.println("You guessed too high, try again? (Y/N)");
+				
+				if(num < secretNum) // entered num is too low
+					System.out.println("You guessed too low, try again? (Y/N)");
+				
+				continuePlaying = scanner.next(); // does user want to continue?
+				
+				if(continuePlaying.equalsIgnoreCase("n")) // user opted to quit
+					userGaveUp = true;
 			}
 			
 		} while(continuePlaying.equalsIgnoreCase("y"));
